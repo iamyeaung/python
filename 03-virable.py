4c8938a45473ef7ef85d68878749b1f200865a5b
Ye Aung <khunyeaung@gmai.com>
2024-12-07 21:55:31 +0630

    virable

---
+++ 03-virable.py
@@ -0,0 +1,22 @@
+#virable
+x = 1           #interger
+y = 1.4         #float
+z = "Ye Aung"   #string
+
+
+print("x result : ", x) # export x data
+print("x result type is : ",type(x)) # check x data type
+
+print("y result : ", y)
+print("y result type is : ", type(y))
+
+print("z result is :", z)
+print("z result type is : ", type(z))
+
+# = : single equal is => assignment operator (သတ်မှတ်ခြင်း)
+# == : double equal is => relational operator (နှိုင်းယှဉ်ခြင်း)
+
+#sometime you can declear as below
+x=y=z= 20
+print("x y z result : " , x,y,z) 
+
