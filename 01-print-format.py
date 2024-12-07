494cca25090bbb30fd3c87975dbae53640fbb96c
Ye Aung <khunyeaung@gmai.com>
2024-12-07 21:53:51 +0630

    print format

---
+++ 01-print-format.py
@@ -0,0 +1,20 @@
+#print
+#format
+
+#print any text by using '' / ""
+print('Hello world')
+print('Hello Python ')
+
+#text format
+print('{2}, {0} live in {1}.'.format('Ye Aung','Hopong','Hello'))
+
+#keyword argument
+print("Hello,I'm {name} & I'm learning {language}.".format(name='Ye Aung',language='Python'))
+
+#print (standard output)
+a = 1.12345
+print('The value of a is %.3f'%a)
+
+a = 1
+b = 2
+print('Total numbers is {}:'.format(a+b))
